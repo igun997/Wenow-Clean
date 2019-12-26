31b23938d64389098dbbb2939ec4958fe477920f
Indra Gunanda <indra.gunanda@gmail.com>
2019-12-26 05:39:05 -0800

    Save State

--- src/js/app.js
+++ src/js/app.js
@@ -20,7 +20,8 @@ var storageSession = store.createInstance({
   name: "session"
 });
 
-var base_url = "http://wenow.id/api/android/";
+// var base_url = "http://wenow.id/api/android/";
+var base_url = "http://127.0.0.1/scm/public/api/android/";
 console.log(storageSession);
 var app = new Framework7({
   root: '#app', // App root element
@@ -41,6 +42,18 @@ var app = new Framework7({
   methods: {
     helloWorld: function () {
       app.dialog.alert('Hello World!');
+    },
+    notifikasi:function(data){
+      console.log(data);
+      var inst = app.notification.create({
+        icon: '<i class="icon demo-icon">W</i>',
+        title: data.title,
+        titleRightText: 'now',
+        subtitle: data.subtitle,
+        text: data.text,
+        closeOnClick: true,
+      })
+      return inst;
     }
   },
   // App routes
@@ -111,6 +124,10 @@ $$("#tab_settings").on("click", function(event) {
   console.log("ClickIt");
   app.views.main.router.navigate("/settings/");
 })
+$$("#tab_help").on("click", function(event) {
+  console.log("ClickIt");
+  app.views.main.router.navigate("/help/");
+})
 $$('#my-login-screen .login-button').on('click', function () {
   var username = $$('#my-login-screen [name="username"]').val();
   var password = $$('#my-login-screen [name="password"]').val();
@@ -146,23 +163,44 @@ var urlBonds = function(add){
 $$('#my-login-screen .register-button').on('click', function () {
   app.loginScreen.close("#my-login-screen");
   app.loginScreen.open("#my-register-screen");
+  var map = L.map('mapid').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      accessToken: 'pk.eyJ1IjoiaWd1bjk5NyIsImEiOiJjazRkNGU2MGEwdHB4M2xwYTU0YWpsd2V4In0.DsoDIY5kzULdad_WuJVyRg'
+  }).addTo(map);
+  map.locate({setView: true, maxZoom: 16});
+  function onLocationFound(e) {
+      var radius = e.accuracy;
+      L.marker(e.latlng).addTo(map)
+          .bindPopup("Lokasi Anda Akurat " + radius + " meter dari titik").openPopup();
+      L.circle(e.latlng, radius).addTo(map);
+      var coord = e.latlng;
+      var lat = coord.lat;
+      var lng = coord.lng;
+      $$('#my-register-screen [name="lat"]').val(lat);
+      $$('#my-register-screen [name="lng"]').val(lng);
+  }
+  map.on('locationfound', onLocationFound);
+  function onLocationError(e) {
+      alert(e.message);
+  }
+
+  map.on('locationerror', onLocationError);
 });
 $$('#my-register-screen .login-button').on('click', function () {
   app.loginScreen.close("#my-register-screen");
   app.loginScreen.open("#my-login-screen");
 });
 $$("#my-register-screen .simpan-button").on("click",function(){
-
-  var getLatLng = urlBonds($$('#my-register-screen [name="alamat"]').val());
-  console.log(getLatLng);
-  app.request.get(getLatLng,function(d,s,x){
-    if (d.status == "OK") {
-      var res = d.results;
       var data = {};
       data.nama = $$('#my-register-screen [name="nama"]').val();
       data.email = $$('#my-register-screen [name="email"]').val();
       data.password = $$('#my-register-screen [name="password"]').val();
       data.jk = $$('#my-register-screen [name="jk"]').val();
+      data.lat = $$('#my-register-screen [name="lat"]').val();
+      data.lng = $$('#my-register-screen [name="lng"]').val();
       data.alamat = $$('#my-register-screen [name="alamat"]').val();
       data.no_hp = $$('#my-register-screen [name="no_hp"]').val();
       app.request.post(base_url+"register",data,function(d, status, xhr){
@@ -177,14 +215,4 @@ $$("#my-register-screen .simpan-button").on("click",function(){
           app.loginScreen.open("#my-login-screen");
         }
       },"json");
-    }else {
-      var data = {
-        title:"Perhatian",
-        subtitle:"Alamat Salah",
-        text:"Alamat anda tidak ditemukan"
-      };
-      notif(data).open();
-    }
-  },"json");
-
 });
--- src/js/routes.js
+++ src/js/routes.js
@@ -4,6 +4,8 @@ import FormPage from '../pages/form.f7.html';
 import PesananPage from '../pages/pesanan.f7.html';
 import PesananDetail from '../pages/pesanan_detail.f7.html';
 import SettingsPage from '../pages/settings.f7.html';
+import HelpPage from '../pages/help.f7.html';
+import AkunPage from '../pages/akun.f7.html';
 import store from 'localforage/dist/localforage.js'
 import LayananPage from '../pages/layanan.f7.html';
 import NotFoundPage from '../pages/404.f7.html';
@@ -109,6 +111,39 @@ var routes = [
     path: '/settings/',
     component: SettingsPage,
   },{
+    path: '/help/',
+    async: function (routeTo, routeFrom, resolve, reject) {
+        var router = this;
+        var app = router.app;
+        var url = app.data.url;
+        var sess = app.data.session;
+        var d = sess.get("data");
+        app.preloader.show();
+        app.request({
+          url: url+"penggunalist",
+          async:false,
+          statusCode: {
+            404: function (xhr) {
+              alert('page not found');
+            }
+          },
+          success:function(d){
+            var pos ;
+            app.preloader.hide();
+            resolve({
+                  component: HelpPage,
+                },{
+                  context: {
+                    pengguna:d
+                  }
+                });
+          },
+          always:function(){
+
+          }
+        });
+    }
+  },{
     path:"/logout/",
     async:function(){
       store.removeItem("isLogin");
@@ -119,6 +154,20 @@ var routes = [
       ses.removeItem("isLogin");
       app.views.main.router.navigate("/");
       app.loginScreen.open("#my-login-screen");
+    }
+  },{
+    path:"/akun/",
+    async:function(routeTo, routeFrom, resolve, reject){
+      var router = this;
+      var app = router.app;
+      var url = app.data.url;
+      var sess = app.data.session;
+      var d = sess.get("data");
+      resolve({ component: AkunPage },{
+            context: {
+              akun:d
+            }
+          });
     }
   }
 
